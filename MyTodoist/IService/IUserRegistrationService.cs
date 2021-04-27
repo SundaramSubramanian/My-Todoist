6577a1d06cec725ad39e7b75d7efd4be62dd5ec2
Sundaram Subramanian <learnwithsundar@gmail.com>
2021-04-27 14:12:13 +0530

    Initial Setup
    Initial Setup and also implemented the User Registration Component

---
+++ MyTodoist/IService/IUserRegistrationService.cs
@@ -0,0 +1,10 @@
+﻿using MyTodoist.Data;
+using System.Threading.Tasks;
+
+namespace MyTodoist.IService
+{
+    public interface IUserRegistrationService
+    {
+        Task<bool> SaveUserRegistration(UserRegistration userRegistration);
+    }
+}
